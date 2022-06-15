c730ffb8a4d8f3cfa555043cdaae35ee6f70f712
alexHoggett <alexmh99@gmail.com>
2022-06-15 11:56:05 +0100

    navbar made sticky

--- js/script.js
+++ js/script.js
@@ -3,10 +3,25 @@
 const burger = document.querySelector('.navbar__burger');
 const overlay = document.querySelector('.navbar__overlay')
 const navlist = document.querySelector('.navbar__list');
+const navbar = document.querySelector('.navbar');
 
 burger.addEventListener('click', (e) => {
   burger.classList.toggle('navbar__burger--active');
   overlay.classList.toggle('navbar__overlay--active');
   document.body.classList.toggle('no-scroll');
   navlist.classList.toggle('navbar__list--active');
-})
+});
+
+// implementing navbar hiding on scroll down
+let lastScrollTop;
+window.addEventListener('scroll', function(){
+  console.log('running');
+  let scrollTop = window.pageYOffset ||
+  this.document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop){
+    navbar.style.top = '-10rem';
+  } else {
+    navbar.style.top = '0';
+  }
+  lastScrollTop = scrollTop;
+});
